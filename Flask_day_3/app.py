7009dea6f1000203cd4b2f34dca19154330fe358
rainsos <rainsos@nate.com>
2025-04-22 15:16:32 +0900

    ✨ Flask  과제 4일차 migrate 설정
    migrate를 설정하여 db와 연동기능 추가 및 upgrade 반영

--- Flask_day_3/app.py
+++ Flask_day_3/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_smorest import Api
 from db import db
 from models import User, Board
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:7722@localhost/oz'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-
+migrate = Migrate(app, db)
 
 # blue print 설정
 app.config["API_TITLE"] = "My API"
